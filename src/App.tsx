30d2dce27902189b93580bf4883832b108999e5c
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-29 14:35:55 -0300

    resolve requisito 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <>
       <h1>Gerenciador de senhas</h1>
-      <div>
-        Hello World
-      </div>
+      <Form />
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,1 +1,28 @@
 import React from 'react';
+
+function Form() {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="Service">Nome do serviço</label>
+        <input type="text" name="Service" id="Service" />
+      </div>
+      <div>
+        <label htmlFor="Login">Login</label>
+        <input type="text" name="Login" id="Login" />
+      </div>
+      <div>
+        <label htmlFor="Senha">Senha</label>
+        <input type="password" name="Senha" id="Senha" />
+      </div>
+      <div>
+        <label htmlFor="URL">URL</label>
+        <input type="text" name="URL" id="URL" />
+      </div>
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </form>
+  );
+}
+
+export default Form;

29ebb04cdc70ef830a11ff95bc36470c9935047a
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-29 15:50:57 -0300

    [Requisito 3]- Renderize condicionalmente o formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
+import React, { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+  const [renderForm, setRenderForm] = useState(false);
+
+  const showForm = () => setRenderForm(true);
+  const hideForm = () => setRenderForm(false);
+
   return (
-    <>
+    <div>
       <h1>Gerenciador de senhas</h1>
-      <Form />
-    </>
+      {/* <Form /> */}
+      {renderForm ? (
+        <Form onCancel={ hideForm } />
+      ) : (
+        <button onClick={ showForm }>Cadastrar nova senha</button>
+      )}
+    </div>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 
-function Form() {
+interface FormProps {
+  onCancel: () => void;
+}
+
+function Form({ onCancel }: FormProps) {
   return (
     <form action="">
+
       <div>
         <label htmlFor="Service">Nome do serviço</label>
         <input type="text" name="Service" id="Service" />
       </div>
+
       <div>
         <label htmlFor="Login">Login</label>
         <input type="text" name="Login" id="Login" />
       </div>
+
       <div>
         <label htmlFor="Senha">Senha</label>
         <input type="password" name="Senha" id="Senha" />
       </div>
+
       <div>
         <label htmlFor="URL">URL</label>
         <input type="text" name="URL" id="URL" />
       </div>
+
       <button>Cadastrar</button>
-      <button>Cancelar</button>
+      <button onClick={ onCancel }>Cancelar</button>
     </form>
   );
 }

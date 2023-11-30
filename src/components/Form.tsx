a754e23aad9597057fe590421666f1559e9bc716
Diogo Pinto <dpintobarbosa@gmail.com>
2023-11-30 17:16:49 -0300

    [corrigindo refatoração]

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 type FormProps = {
   onCancel: () => void;
-  // registerPassword: (event: NewRegister) => void;
+  registerPassword: (event: NewRegister) => void;
 };
 
 type NewRegister = {
@@ -12,12 +12,11 @@ type NewRegister = {
   url: string;
 };
 
-function Form({ onCancel }: FormProps) {
+function Form({ onCancel, registerPassword }: FormProps) {
   const [service, setService] = useState('');
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [url, setUrl] = useState('');
-  const [renderForm, setRenderForm] = useState(false);
   const [buttonEnable, setButtonEnable] = useState(false);
   const [registers, setRegister] = useState<NewRegister[]>([]);
 
@@ -32,10 +31,6 @@ function Form({ onCancel }: FormProps) {
 
   const valid = 'valid-password-check';
   const invalid = 'invalid-password-check';
-
-  const toggleForm = () => {
-    setRenderForm(!renderForm);
-  };
 
   const registering = (event: React.FormEvent) => {
     event.preventDefault();
@@ -54,96 +49,92 @@ function Form({ onCancel }: FormProps) {
 
   return (
     <div>
-      {!renderForm ? (
-        <button onClick={ toggleForm }>Cadastrar nova senha</button>
-      ) : (
-        <form onSubmit={ registering }>
-          <div>
-            <label htmlFor="Service">Nome do serviço</label>
-            <input
-              type="text"
-              name="Service"
-              id="Service"
-              value={ service }
-              onChange={ (event) => setService(event.target.value) }
-            />
-          </div>
+      <form onSubmit={ registering }>
+        <div>
+          <label htmlFor="Service">Nome do serviço</label>
+          <input
+            type="text"
+            name="Service"
+            id="Service"
+            value={ service }
+            onChange={ (event) => setService(event.target.value) }
+          />
+        </div>
 
-          <div>
-            <label htmlFor="Login">Login</label>
-            <input
-              type="text"
-              name="Login"
-              id="Login"
-              value={ login }
-              onChange={ (event) => setLogin(event.target.value) }
-            />
-          </div>
+        <div>
+          <label htmlFor="Login">Login</label>
+          <input
+            type="text"
+            name="Login"
+            id="Login"
+            value={ login }
+            onChange={ (event) => setLogin(event.target.value) }
+          />
+        </div>
 
-          <div>
-            <label htmlFor="Senha">Senha</label>
-            <input
-              type="password"
-              name="Senha"
-              id="Senha"
-              minLength={ 8 }
-              maxLength={ 16 }
-              value={ password }
-              onChange={ (event) => {
-                setPassword(event.target.value);
-                handleChangePassword();
-              } }
-            />
-          </div>
+        <div>
+          <label htmlFor="Senha">Senha</label>
+          <input
+            type="password"
+            name="Senha"
+            id="Senha"
+            minLength={ 8 }
+            maxLength={ 16 }
+            value={ password }
+            onChange={ (event) => {
+              setPassword(event.target.value);
+              handleChangePassword();
+            } }
+          />
+        </div>
 
-          <div>
-            <p className={ password.length < 8 ? invalid : valid }>
-              Possuir 8 ou mais caracteres
-            </p>
+        <div>
+          <p className={ password.length < 8 ? invalid : valid }>
+            Possuir 8 ou mais caracteres
+          </p>
 
-            <p className={ password.length < 16 ? valid : invalid }>
-              Possuir até 16 caracteres
-            </p>
+          <p className={ password.length < 16 ? valid : invalid }>
+            Possuir até 16 caracteres
+          </p>
 
-            <p
-              className={
-                /[a-zA-z]+/.test(password) && /\d/.test(password)
-                  ? valid
-                  : invalid
-              }
-            >
-              Possuir letras e números
-            </p>
-
-            <p
-              className={
-                /[!@#$%^&*(),.?":{}|<>]/.test(password) ? valid : invalid
-              }
-            >
-              Possuir algum caractere especial
-            </p>
-          </div>
-
-          <div>
-            <label htmlFor="URL">URL</label>
-            <input
-              type="text"
-              name="URL"
-              id="URL"
-              value={ url }
-              onChange={ (event) => setUrl(event.target.value) }
-            />
-          </div>
-
-          <button
-            type="submit"
-            disabled={ !service || !login || !buttonEnable || !url }
+          <p
+            className={
+              /[a-zA-z]+/.test(password) && /\d/.test(password)
+                ? valid
+                : invalid
+            }
           >
-            Cadastrar
-          </button>
-          <button onClick={ onCancel }>Cancelar</button>
-        </form>
-      )}
+            Possuir letras e números
+          </p>
+
+          <p
+            className={
+              /[!@#$%^&*(),.?":{}|<>]/.test(password) ? valid : invalid
+            }
+          >
+            Possuir algum caractere especial
+          </p>
+        </div>
+
+        <div>
+          <label htmlFor="URL">URL</label>
+          <input
+            type="text"
+            name="URL"
+            id="URL"
+            value={ url }
+            onChange={ (event) => setUrl(event.target.value) }
+          />
+        </div>
+
+        <button
+          type="submit"
+          disabled={ !service || !login || !buttonEnable || !url }
+        >
+          Cadastrar
+        </button>
+        <button onClick={ onCancel }>Cancelar</button>
+      </form>
       { registers.length === 0 ? (
         <p>Nenhuma senha cadastrada</p>
       ) : (
@@ -157,11 +148,6 @@ function Form({ onCancel }: FormProps) {
                   Login
                   {' '}
                   { register.login }
-                </p>
-                <p>
-                  Senha
-                  {' '}
-                  { register.password }
                 </p>
               </li>
             ))
